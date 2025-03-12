b96367d062c6700edcb762c3b09f7d03a95de182
Assia182 <hannouassia1@gmail.com>
2025-03-12 10:50:15 +0100

    initialisation Xcode

---
+++ Jeutaro/Jeutaro/JeutaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeutaroApp.swift
+//  Jeutaro
+//
+//  Created by etud on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct JeutaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
